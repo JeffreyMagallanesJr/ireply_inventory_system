a8ec2de7f282ea076feec697020684d2cca1efcc
Jeffrey C. Magallanes Jr. <jeffreycmagjr@gmail.com>
2025-03-10 12:27:40 +0800

    Add inventory and items in sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Monitor, UserCircle, } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, Monitor, UserCircle, ClipboardList, Smartphone } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -22,6 +22,16 @@ const mainNavItems: NavItem[] = [
         title: 'Equipment',
         url: '/equipment',
         icon: Monitor,
+    },
+    {
+        title: 'Inventory',
+        url: '/inventory',
+        icon: ClipboardList,
+    },
+    {
+        title: 'Items',
+        url: '/items',
+        icon: Smartphone,
     },
 ];
 
